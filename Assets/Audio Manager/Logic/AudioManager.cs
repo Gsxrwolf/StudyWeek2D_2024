f7adf8a55fc5f4cceb36f611a11143c91b4a4bd9
Frederic Faschina <faschina.frederic@icloud.com>
2024-05-22 00:44:26 +0200

    Internal Logic & Cleanup

--- Assets/Audio Manager/Logic/AudioManager.cs
+++ Assets/Audio Manager/Logic/AudioManager.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    #region Editor Settings
+    [Header("Background Music")]
+
+    // Private Member -> Should play bg music
+    [SerializeField] private bool _backgroundMusic = true;
+
+    // Private Member -> List of background tracks
+    [SerializeField] private List<AudioClip> _backgroundTracks = new List<AudioClip>();
+
+    // Private Memeber -> Pause between tracks
+    [SerializeField] private int _loopPause = 1;
+
+
+    [Space()]
+
+
+    [Header("Audio Source")]
+
     // Private Member -> Music Source
     [SerializeField] private AudioSource _musicSource;
 
@@ -12,6 +30,8 @@ public class AudioManager : MonoBehaviour
 
     // Private Mmeber -> Effects Source
     [SerializeField] private AudioSource _effectsSource;
+
+    #endregion
 
 
 
@@ -22,6 +42,12 @@ public class AudioManager : MonoBehaviour
 
         // Validate Source´s
         ValidateSources();
+    }
+
+    private void Start()
+    {
+        // Start Background Music
+        StartCoroutine(BackgroundMusicRoutine());
     }
 
     #region Singleton Pattern
@@ -50,7 +76,6 @@ public class AudioManager : MonoBehaviour
     #endregion
 
     #region Source Management
-
     /// <summary>
     /// This function makes sure that all source´s are valid.
     /// Invalid sources will be replaced by a new source.
@@ -102,5 +127,83 @@ public class AudioManager : MonoBehaviour
     {
         return _effectsSource is not null;
     }
+    #endregion
+
+    #region Public Functions
+
+    /// <summary>
+    /// Allows and starts background music.
+    /// </summary>
+    public void StartBackgroundMusic()
+    {
+        // Endable background music
+        _backgroundMusic = true;
+
+        // Start routine
+        StartCoroutine(BackgroundMusicRoutine());
+    }
+
+    /// <summary>
+    /// Stops the background music.
+    /// </summary>
+    /// <param name="finish">Should wait for current track to end?</param>
+    public void StopBackgroundMusic(bool finish = false)
+    {
+        // Stop the routine
+        StopCoroutine(BackgroundMusicRoutine());
+
+        // Make sure source does not loop
+        _musicSource.loop = false;
+
+        // Disable background music
+        _backgroundMusic = false;
+
+        
+        if(finish is false)
+        {
+            // Stop the source immediatly
+            _musicSource.Stop();
+
+            // End function
+            return;
+        }
+    }
+
+    #endregion
+
+    #region Routines
+    /// <summary>
+    /// Runs in the "background" (No multithreading) and manages the
+    /// background music. Randomly selects next track, makes pauses etc ...
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator BackgroundMusicRoutine()
+    {
+        // Local Memeber: Current Playing Track (Placeholder)
+        AudioClip audioClip = null;
+
+        // Loop -> While music should play and tracks are available
+        while(_backgroundMusic && _backgroundTracks.Count > 0)
+        {
+            // Local Member -> Select Random Track
+            AudioClip selection = _backgroundTracks[Random.Range(0, _backgroundTracks.Count - 1)];
+
+            // End Loop -> if there are more than 2 tracks, end loop if selection is last played
+            if(_backgroundTracks.Count >= 2 && selection == audioClip) { yield return null; }
+
+            // Play new clip
+            audioClip = selection;
+            _musicSource.clip = audioClip;
+            _musicSource.loop = false;
+            _musicSource.Play();
+
+            // Debug -> New track selected
+            Debug.Log("Background Music: New track selected!");
+
+            // Routine Return -> Wait for end of track (+ pause)
+            yield return new WaitForSeconds(audioClip.length + _loopPause);
+        }
+    }
+
     #endregion
 }
