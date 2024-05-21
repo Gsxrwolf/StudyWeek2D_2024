6852aa015519b26441af55e31365d4c3fb406bc7
Frederic Faschina <faschina.frederic@icloud.com>
2024-05-21 23:41:37 +0200

    Audio Manager - Grundstein
    Folders / Singleton Pattern Class / Prefab / Audio Mixer

---
+++ Assets/Audio Manager/Logic/AudioManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Static Access (Can only be set from inside the class) -> Property
+    public static AudioManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        // Make sure this class is no duplicate
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        // If no duplicate -> Set instance to this class
+        Instance = this;
+    }
+}
